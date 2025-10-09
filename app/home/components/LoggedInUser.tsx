4d598b8c32de308663226b686bbc12b6460a1af8
IDeal <cyon256@icloud.com>
2025-10-10 06:49:23 +0900

    loggedIn user

--- app/home/components/LoggedInUser.tsx
+++ app/home/components/LoggedInUser.tsx
@@ -1,41 +1,37 @@
 "use client";
-import Book from "@/app/components/Book";
-import Docs from "@/app/components/Docs";
 import { LoginJWTPayload, logoutAction } from "@/app/lib/actions";
-import { blackHanSans } from "@/app/stores/font";
+import { blackHanSans, notosansKorean, notosansKorean_500 } from "@/app/stores/font";
 import Link from "next/link";
+import {BookImage, LogOut, BrainCircuit, FileText, Rocket, User } from "lucide-react";
 
 export default function LoggedInUser({user}:{user:LoginJWTPayload}) {
 	const {userId, name} = user
 
-	return <div className={`${blackHanSans.className} text-sm select-none grid grid-cols-3 grid-rows-2  gap-3  items-center h-44 p-3 bg-amber-50 rounded-md`}>
-		<div className="text-sm col-span-1 h-full rounded-md bg-amber-400">
-			<Link href={`/todo/${userId}`} >
-			{name} 오늘의 과제
+	return <div className={`${notosansKorean_500.className} text-xs select-none grid grid-cols-3 grid-rows-2 gap-2 items-center h-44 p-3 bg-amber-50 rounded-md`}>
+			<Link href={`/todo/${userId}`} 
+			className=" flex flex-col justify-center items-center col-span-1 h-full gap-2  rounded-md hover:bg-amber-400 hover:text-amber-50 p-2">
+				<Rocket className="w-10 h-10 stroke-1 " />
+				<div> {name}의 과제 </div>
 			</Link>
-			</div>
 			<Link href={`/report/${userId}`}  
-			className="flex flex-col items-center justify-center col-span-1 h-full bg-amber-300 rounded-md">
-				<Book className="stroke-1 w-24 h-24"/>
-				<div className=""> 교재방 </div>
+			className="flex flex-col gap-2 items-center justify-center col-span-1 h-full  rounded-md hover:bg-amber-400 hover:text-amber-50 p-2">
+				<BookImage className="stroke-1 w-12 h-12"/>
+				<div className=""> 학원 교재방 </div>
 			</Link>
-		<div className="col-span-1 h-full bg-amber-300 rounded-md"> 
-			<Link href={`/mindmap/${userId}`} >
-				마인드맵
+			<Link href={`/mindmap/${userId}`} className="p-2 flex flex-col gap-2 justify-center items-center col-span-1 h-full  rounded-md hover:bg-amber-400 hover:text-amber-50" >
+				<BrainCircuit className="w-12 h-12 stroke-1" />
+				<div>{name}의 마인드맵 </div>
 			</Link>
-		</div>
-		<div className="col-span-1 h-full bg-amber-300 rounded-md">
-			<Link href={`/mypage/${userId}`} >
-			 마이 페이지
+			<Link href={`/mypage/${userId}`} className="p-2 col-span-1 gap-2 h-full  rounded-md flex-col flex justify-center items-center hover:bg-amber-400 hover:text-amber-50" >
+				<User className="stroke-1 w-12 h-12"/>{name} 페이지
 			</Link>
-		</div>
 			<Link href={`/report/${userId}`}  
-			className="flex flex-col items-center justify-center col-span-1 h-full bg-amber-300 rounded-md">
-				<Docs className="stroke-1 w-24 h-24"/>
-				<div className=""> {name} 보고서 </div>
+			className="p-2 flex flex-col items-center justify-center col-span-1 gap-2 h-full  rounded-md hover:bg-amber-400 hover:text-amber-50">
+				<FileText className="stroke-1 w-12	h-12"/>
+				<div className=""> {name}의 보고서 </div>
 			</Link>
-		<div className="col-span-1 h-full bg-amber-300 rounded-md" 
-		onClick={async()=> await logoutAction()}> 로그아웃 </div>
+		<div className="p-2 col-span-1 h-full  rounded-md flex flex-col justify-center gap-2 items-center hover:bg-amber-400 hover:text-amber-50" 
+		onClick={async()=> await logoutAction()}> <LogOut className="w-12 h-12 stroke-1"/> 로그아웃 </div>
 	</div>
 }
 
--- app/stores/font.ts
+++ app/stores/font.ts
@@ -2,3 +2,4 @@ import { Black_Han_Sans, Noto_Sans_KR, Sunflower } from "next/font/google"
 export const blackHanSans = Black_Han_Sans({weight: "400"})
 export const sunflower  = Sunflower({weight: "300"})
 export const notosansKorean = Noto_Sans_KR({weight: "300"})
+export const notosansKorean_500 = Noto_Sans_KR({weight: "500"})
