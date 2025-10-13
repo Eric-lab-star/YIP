8da875fe71f94c4953c0e44db850e727358e6199
IDeal <cyon256@icloud.com>
2025-10-14 04:05:54 +0900

    delete unused path

--- app/myPage/page.tsx
+++
@@ -1,8 +0,0 @@
-export default async function Page(
-) {
-	return (
-		<div>
-			my page
-		</div>
-	)
-}
--- app/projects/ddokddokboard/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Page( ) {
-	return (
-		<div>
-		똑똑보드 
-		</div>
-	)
-}
