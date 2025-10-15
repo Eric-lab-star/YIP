5579572023e08878f4662b5c791e56e5acb7e236
Eric Kim <cyon256@icloud.com>
2025-10-15 13:34:08 +0900

    todo

--- app/todo/page.tsx
+++ app/todo/page.tsx
@@ -1,10 +1,9 @@
-import { joinProjToUser } from "@/app/lib/projToUser";
-import { blackHanSans, notosansKorean, notosansKorean_500 } from "@/app/stores/font";
 
 export default async function Page() {
 		return (
 			<div>
-				todo
+				todo 
+				<div>written from dev branch</div>
 			</div>
 		)
 }
