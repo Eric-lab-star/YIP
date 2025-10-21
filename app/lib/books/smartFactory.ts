89153408058a140136fc21c6dd60e31384382f31
Eric Kim <cyon256@icloud.com>
2025-10-21 10:07:58 +0900

    r2 mongo

--- app/lib/books/smartFactory.ts
+++ app/lib/books/smartFactory.ts
@@ -1,5 +1,6 @@
 import { ListObjectsV2Command } from "@aws-sdk/client-s3";
 import { r2client } from "../r2";
+import { getDB } from "../db";
 
 /** Upload function upload r2 image key and blur data url to mongodb
 	*
@@ -11,5 +12,21 @@ export async function upload() {
 	})
 
 	const {Contents} = await r2client.send(command)
-	console.log(Contents)
+
+
+	if (!Contents) return;
+
+	const db = await getDB();
+	const coll = await db.createCollection("smartFactoryIntro")
+	
+	const docs = [];
+
+	for (let i = 0; i < Contents.length; i++) {
+		const doc = {
+			key: Contents[i].Key,
+		}
+		docs.push(doc)
+	}
+	const {insertedCount} = await coll.insertMany(docs) 
+	console.log(insertedCount);
 }
