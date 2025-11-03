26b3fec7ca4ed2335fb922119b0476620635d984
IDeal <cyon256@icloud.com>
2025-11-04 00:01:01 +0900

    expanding db

--- app/lib/mongo/students.ts
+++ app/lib/mongo/students.ts
@@ -1,14 +1,17 @@
 import { StudentData } from "../zod/studentSchema";
 import { getDB } from "./db";
 
-
-
+/*
+	* calls mongodb and create new student doc
+	* */
 export async function createStudent(student: StudentData){
 	const db = await getDB();
 	const students = db.collection<StudentData>("students")
 	return await students.insertOne(student)
 }
-
+/*
+	* calls mongodb and get all students doc
+	*/
 export async function readManyStudent(){
 	const db = await getDB();
 	const student = db.collection<StudentData>("students")
--- app/page.tsx
+++ app/page.tsx
@@ -14,7 +14,6 @@ import { mutate } from "swr";
 export default function Page() {
 	const [isPending, startTransition ] = useTransition();
 	const [serverResult, setServerResult] = useState<responseType>({})
-	
 
 	const { register, handleSubmit, formState:{errors}, reset  } = useForm<StudentData>({
 		resolver: zodResolver(studentSchema)
@@ -24,31 +23,55 @@ export default function Page() {
 		startTransition(async () => {
 			const res = await postStudent(data)
 			setServerResult(res)
-			if (res.success) reset();
-			mutate("/api/students")
+			if (res.success) {
+				reset();
+				mutate("/api/students")
+			}
 		})
 	}
 
 
 	return (
 		<div>
-			<form onSubmit={handleSubmit(onSubmit)}  className="bg-amber-100 pt-3 flex justify-center items-center space-y-1 flex-col">
-				<input  placeholder="이름을 입력하세요"  className={"p-2 w-150 bg-amber-50"} {...register("name")}  />
+			<form onSubmit={handleSubmit(onSubmit)}  className={formStyle()} >
+				<input  placeholder="이름을 입력하세요"  className={inputStyle({size: "l"})} {...register("name", {required: true})}  />
 				<div className="flex flex-wrap">
-					<input placeholder="년"  className={"p-2 w-50 bg-amber-50"}  {...register("birthYear")}/>
-					<input placeholder="월"  className={"p-2 w-50 bg-amber-50"} {...register("birthMonth")}/>
-					<input placeholder="일" className={"p-2 w-50 bg-amber-50"}   { ...register("birthDate") }/>
+					<input placeholder="년"  className={inputStyle({size: "s"})}  {...register("birthYear", {required: true})}/>
+					<input placeholder="월"  className={inputStyle({size: "s"})} {...register("birthMonth", {required:true })}/>
+					<input placeholder="일" className={inputStyle({size: "s"})}   { ...register("birthDate", {required: true}) }/>
 				</div>
-				<input placeholder="학교" className={"p-2 w-150 bg-amber-50 "}   {...register("school")} />
-				<input type="submit" className="p-2 border-2 rounded-2xl"/>
+				<input placeholder="학교" className={inputStyle({size: "l"})}   {...register("school", {required: "학교를 입력하세요"})} />
+				<input placeholder="" className={inputStyle({size: "l"})} {...register("attend", {required: true})}/>
+				<input type="submit" defaultValue={isPending ? "업로딩" : "제출"} className={submitStyle()} disabled={isPending}/>
+			{serverResult.errors && <div>{serverResult.errors.toString()}</div>}
+			{errors.name && <div>이름을 입력하세요</div>}
+			{errors.birthYear && <div>생년월일을 확인하세요</div>}
+			{errors.birthMonth && <div>생년월일을 확인하세요</div>}
+			{errors.birthDate && <div>생년월일을 확인하세요</div>}
+			{errors.school && <div>학교를 입력하세요</div>}
 			</form>
 			<StudentList />
 		</div>
 	)
 }
 
+const submitStyle = tv({
+	base: "w-150 shadow-2xl  p-2 bg-amber-50 mb-2 rounded-2xl"
+})
+
 const inputStyle = tv({
-	base: "p-2 bg-amber-50"
+	base: "p-2 bg-amber-50",
+	variants: {
+		size: {
+			s: "w-50",
+			r: "w-100",
+			l: "w-150"
+		}
+	}
+})
+
+const formStyle = tv({
+	base: "bg-amber-100 pt-3 flex justify-center items-center space-y-1 flex-col"
 })
 
 
