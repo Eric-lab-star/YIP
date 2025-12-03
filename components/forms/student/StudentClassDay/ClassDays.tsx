0fdb000dbf1cf932f950f059f5bb50f8fddf04e8
IDeal <cyon256@icloud.com>
2025-12-03 11:42:12 +0900

    css

--- components/forms/student/StudentClassDay/ClassDays.tsx
+++ components/forms/student/StudentClassDay/ClassDays.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { container, label } from "@/app/lib/tv/forms/FormStyles";
+import { container, input, label } from "@/app/lib/tv/forms/FormStyles";
 import { Plus } from "lucide-react";
 import { useClassDays } from "@/app/stores/classDayStore";
 import ClassDaySelect from "./ClassDaySelect";
@@ -12,6 +12,7 @@ import { tv } from "tailwind-variants";
 */
 export default function ClassDays() {
 	const { addDay, days } = useClassDays();
+	const {button} = input({width: "f"});
 	return (
 		<div className={container({className: "my-3"})}>
 			<div className={label()}>등원일</div>
@@ -20,12 +21,9 @@ export default function ClassDays() {
 			</div>
 			<div className={button()} onClick={()=> addDay()}>
 				<div className="pr-3">등원일 추가</div>
-				<Plus className="text-amber-400" /> 
+				<Plus /> 
 			</div>
 		</div>
 	)
 }
 
-const button = tv({
-	base: "p-2  hover:bg-amber-300 flex justify-center items-center bg-amber-200 rounded-xl" 
-})
