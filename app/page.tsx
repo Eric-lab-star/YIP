983e2b795cbc2f795223b340af910c0fe1d4ee08
IDeal <cyon256@icloud.com>
2025-12-10 10:23:10 +0900

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import Link from "next/link";
 
 //TODO: UPdate StudentInfo -> populate classDay
 export default function Page() {
-	return <div>
+	return <div className="flex flex-col">
+		<Link href={"/students"} > students </Link>
+		<Link href={"/students/create"} > create </Link>
 		page
 	</div>
 }
