96a67d017a1ac98953f481338d82bc79588af8dd
Eric Kim <cyon256@icloud.com>
2025-12-13 16:41:01 +0900

    mac

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,11 @@
 import Link from "next/link";
 
-//TODO: Update StudentInfo -> populate classDay
 // FIX: classDay state type should be more precise
+// FIX: Update page button 
 export default function Page() {
 	return <div className="flex flex-col">
 		<Link href={"/students"} > students </Link>
 		<Link href={"/students/create"} > create </Link>
-		page
 	</div>
 }
 
