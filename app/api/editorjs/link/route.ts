e013044c387815783c7019921f6e374483abe90c
IDeal <cyon256@icloud.com>
2026-02-11 14:03:08 +0900

    editor

--- app/api/editorjs/link/route.ts
+++ app/api/editorjs/link/route.ts
@@ -1,20 +1,59 @@
+import og from "open-graph-scraper";
 import { NextRequest } from "next/server";
 
 export async function GET(req: NextRequest) {
-	console.log(req)
 	const param = req.nextUrl.searchParams
-	const link = param.get("url")
+	const link = validateURL(param)
+	if (!(typeof link == "string")) {
+		return link
+	}
 
+	const {error, result} = await og({url: link})
+	console.log(error, result.ogTitle, result.ogDescription, result.ogImage)
+
+	if (error) {
+		return Response.json({
+			success: 1,
+			link,
+		})
+	}
 
 	return Response.json({
 		success : 1,
 		link,
 		meta: {
-			title: "this is name of site",
-			description: "blah blah blah",
-			image: {
-				url: "https://codex.so/public/app/img/meta_img.png",
-			}
+			title: result.ogTitle,
+			description: result.ogDescription,
+			image: result.ogImage && result.ogImage[0] 
 		}
 	})
+}
+
+
+function validateURL(param: URLSearchParams) {
+	try {
+
+		const link  = param.get("url")
+		if (!link) {
+			return Response.json({
+				success: 0,
+				error: "Missing URL",
+			},{status: 400})
+		}
+
+		const url = new URL(link);
+		if (!["http:", "https:"].includes(url.protocol)) {
+			return Response.json({
+				success: 0,
+				error: "Invliad protocol",
+			},{status: 400})
+		}
+		return link
+	} catch(e) {
+		return Response.json({
+			success: 0,
+			errro: "Invalid URL",
+		},{status:400})
+	}
+
 }
