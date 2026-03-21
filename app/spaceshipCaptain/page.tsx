5614cd2544bb8129bc57f70f99c26e31d7481a84
Eric Kim <cyon256@icloud.com>
2026-03-22 03:44:22 +0900

    game

--- app/spaceshipCaptain/page.tsx
+++ app/spaceshipCaptain/page.tsx
@@ -1,6 +1,14 @@
 
 export default function Page() {
 	return <div>
-		game
+    <div className="flex items-center justify-center min-h-screen bg-black">
+      <iframe
+        src="/game/index.html"
+        width={450}
+        height={800}
+        style={{ border: "none" }}
+        allow="autoplay"
+      />
+    </div>
 	</div>
 }
