80621d736c8039e6341939026b60ab9830da6813
Eric Kim <cyon256@icloud.com>
2026-03-23 18:12:24 +0900

    space shooter

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+	async headers() {
+		return [
+      {
+        source: '/game/:path*',
+        headers: [
+          { key: 'Cross-Origin-Opener-Policy', value: 'same-origin' },
+          { key: 'Cross-Origin-Embedder-Policy', value: 'require-corp' },
+        ],
+      },
+    ];
+	},
 	reactStrictMode: false,
 	images: {
 		remotePatterns: [
