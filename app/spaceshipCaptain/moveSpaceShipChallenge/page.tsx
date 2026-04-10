7407b273b94030c492f2cf990a74962a5fdb42db
Eric Kim <cyon256@icloud.com>
2026-04-11 04:50:09 +0900

    move space ship

---
+++ app/spaceshipCaptain/moveSpaceShipChallenge/page.tsx
@@ -0,0 +1,16 @@
+import Text from "@/components/commons/Text";
+import Title from "@/components/commons/Title";
+
+export default function Page() {
+  return (
+    <div className="p-10 mb-100">
+      <Title size="h2"> 우주선 이동시키기 과제 </Title>
+      <Text my="m">
+        현제 우리가 작성한 코드는 우주선이 좌우로만 이동하다가 벽을 만나면
+        방향을 바꾸도록 프로그래밍 되어있어요. 우리는 여기에서 한 단계 더
+        발전시켜볼거예요. 이제는 x, y 모든 방향으로 이동하게 만들고 어떤 벽이든
+        벽을 만나면 방향을 반대로 바꿔줄 거예요.
+      </Text>
+    </div>
+  );
+}
--- utils/sideBarTree/spaceshipCaptainTree.ts
+++ utils/sideBarTree/spaceshipCaptainTree.ts
@@ -1,125 +1,148 @@
 import { SideBarTreeItem } from "@/components/commons/SideBarItems";
 
 export const spaceshipCaptainTree: SideBarTreeItem[] = [
-	{
-		kind: "folder",
-		name: "프로젝트 설정하기",
-		files: [
-			{
-				kind: "file",
-				name: "uv 설치",
-				url: "/spaceshipCaptain/setup#uv_setup"
-			},
-			{
-				kind: "file",
-				name: "uv 실행",
-				url: "/spaceshipCaptain/setup#uv_init"
-			},
-			{
-				kind: "file",
-				name: "pygame-ce 설치하기",
-				url: "/spaceshipCaptain/setup#pygame-ce"
-			},
-			{
-				kind: "file",
-				name: "main.py: 파이게임 초기화",
-				url: "/spaceshipCaptain/setup#pygame-init",
-			},
-			{
-				kind: "file",
-				name: "main.py: display_surface",
-				url: "/spaceshipCaptain/setup#display_surface",
-			},
-			{
-				kind: "file",
-				name: "main.py: while 루프",
-				url: "/spaceshipCaptain/setup#while-setup",
-			},
-			{
-				kind: "file",
-				name: "main.py: 이벤트",
-				url: "/spaceshipCaptain/setup#event"
-			},
-			{
-				kind: "file",
-				name: "결과 확인하기",
-				url: "/spaceshipCaptain/setup#result",
-			}
-		]
-	},
+  {
+    kind: "folder",
+    name: "프로젝트 설정하기",
+    files: [
+      {
+        kind: "file",
+        name: "uv 설치",
+        url: "/spaceshipCaptain/setup#uv_setup",
+      },
+      {
+        kind: "file",
+        name: "uv 실행",
+        url: "/spaceshipCaptain/setup#uv_init",
+      },
+      {
+        kind: "file",
+        name: "pygame-ce 설치하기",
+        url: "/spaceshipCaptain/setup#pygame-ce",
+      },
+      {
+        kind: "file",
+        name: "main.py: 파이게임 초기화",
+        url: "/spaceshipCaptain/setup#pygame-init",
+      },
+      {
+        kind: "file",
+        name: "main.py: display_surface",
+        url: "/spaceshipCaptain/setup#display_surface",
+      },
+      {
+        kind: "file",
+        name: "main.py: while 루프",
+        url: "/spaceshipCaptain/setup#while-setup",
+      },
+      {
+        kind: "file",
+        name: "main.py: 이벤트",
+        url: "/spaceshipCaptain/setup#event",
+      },
+      {
+        kind: "file",
+        name: "결과 확인하기",
+        url: "/spaceshipCaptain/setup#result",
+      },
+    ],
+  },
 
-	{
-		kind: "folder",
-		name: "surface 이해하기",
-		files: [
-			{
-				kind: "file",
-				name: "창: display",
-				url: "/spaceshipCaptain/surface/#display"
-			},
-			{
-				kind: "file",
-				name: "창 제목: caption",
-				url: "/spaceshipCaptain/surface/#caption"
-			},
-			{
-				kind: "file",
-				name: "창 업데이트: flip()",
-				url: "/spaceshipCaptain/surface/#flip"
-			},
-			{
-				kind: "file",
-				name: "창 색칠하기: fill()",
-				url: "/spaceshipCaptain/surface/#fill"
-			},
-			{
-				kind: "file",
-				name: "모양 추가하기: Surface()",
-				url: "/spaceshipCaptain/surface/#surface"
-			},
-			{
-				kind: "file",
-				name: "결과 확인하기",
-				url: "/spaceshipCaptain/surface/#result"
-			},
-
-		],
-	},
-	{
-		kind: "file",
-		name: "색바꾸기 과제",
-		url: "/spaceshipCaptain/surface_challenge",
-	},
-	{
-		kind: "folder",
-		name: "우주선 조종하기",
-		files: [
-			{
-				kind: "file",
-				name: "이미지 저장하기",
-				url: "/spaceshipCaptain/playerSurf#download",
-			},
-			{
-				kind: "file",
-				name: "이미지 코드",
-				url: "/spaceshipCaptain/playerSurf#image_surf",
-			},
-			{
-				kind: "file",
-				name: "이미지 코드 점검",
-				url: "/spaceshipCaptain/playerSurf#checkpoint1",
-			},
-			{
-				kind: "file",
-				name: "연습하기",
-				url: "/spaceshipCaptain/playerSurf#practice",
-			},
-			{
-				kind: "file",
-				name: "배경 추가하기",
-				url: "/spaceshipCaptain/playerSurf#addingBG",
-			},
-		],
-	}
-
-]
+  {
+    kind: "folder",
+    name: "surface 이해하기",
+    files: [
+      {
+        kind: "file",
+        name: "창: display",
+        url: "/spaceshipCaptain/surface/#display",
+      },
+      {
+        kind: "file",
+        name: "창 제목: caption",
+        url: "/spaceshipCaptain/surface/#caption",
+      },
+      {
+        kind: "file",
+        name: "창 업데이트: flip()",
+        url: "/spaceshipCaptain/surface/#flip",
+      },
+      {
+        kind: "file",
+        name: "창 색칠하기: fill()",
+        url: "/spaceshipCaptain/surface/#fill",
+      },
+      {
+        kind: "file",
+        name: "모양 추가하기: Surface()",
+        url: "/spaceshipCaptain/surface/#surface",
+      },
+      {
+        kind: "file",
+        name: "결과 확인하기",
+        url: "/spaceshipCaptain/surface/#result",
+      },
+    ],
+  },
+  {
+    kind: "file",
+    name: "색바꾸기 과제",
+    url: "/spaceshipCaptain/surface_challenge",
+  },
+  {
+    kind: "folder",
+    name: "우주선 이미지",
+    files: [
+      {
+        kind: "file",
+        name: "이미지 저장하기",
+        url: "/spaceshipCaptain/playerSurf#download",
+      },
+      {
+        kind: "file",
+        name: "이미지 코드",
+        url: "/spaceshipCaptain/playerSurf#image_surf",
+      },
+      {
+        kind: "file",
+        name: "이미지 코드 점검",
+        url: "/spaceshipCaptain/playerSurf#checkpoint1",
+      },
+      {
+        kind: "file",
+        name: "연습하기",
+        url: "/spaceshipCaptain/playerSurf#practice",
+      },
+      {
+        kind: "file",
+        name: "배경 추가하기",
+        url: "/spaceshipCaptain/playerSurf#addingBG",
+      },
+      {
+        kind: "file",
+        name: "Frect",
+        url: "/spaceshipCaptain/playerSurf#getFrect",
+      },
+      {
+        kind: "file",
+        name: "Center Frect ",
+        url: "/spaceshipCaptain/playerSurf#frectCenter",
+      },
+      {
+        kind: "file",
+        name: "Move Frect",
+        url: "/spaceshipCaptain/playerSurf#moveFrect",
+      },
+      {
+        kind: "file",
+        name: "확인하기",
+        url: "/spaceshipCaptain/playerSurf#result",
+      },
+    ],
+  },
+  {
+    kind: "file",
+    name: "우주선 이동시키기 과제",
+    url: "/spaceshipCaptain/moveSpaceShipChallenge",
+  },
+];
