c41bc9d495b6d149b83a4f40feca236f0bb1d502
Eric Kim <cyon256@icloud.com>
2026-04-17 15:08:23 +0900

    check

--- components/commons/References.tsx
+++ components/commons/References.tsx
@@ -1,25 +1,35 @@
-import Link from "next/link"
-import Text from "./Text"
-import Title from "./Title"
-import { Link2 } from "lucide-react"
+import Link from "next/link";
+import Text from "./Text";
+import Title from "./Title";
+import { Link2 } from "lucide-react";
 
-export default function References({ moreLinks }: { moreLinks: { label: string, src: string }[] }) {
-
-
-	return <>
-		<Title my="l" size="h2">관련 자료</Title>
-		{
-			moreLinks.map(
-				(v, i) => <Text key={i}>
-					<Link target="_blank" href={v.src} className="w-fit flex flex-col hover:font-semibold hover:text-blue-900">
-						<div className="">{i + 1}. {v.label} : </div>
-						<div className="ml-10"><Link2 size="19" className="inline-block mr-2" /> {v.src}</div>
-					</Link>
-				</Text>)
-		}
-	</>
-
+export default function References({
+  moreLinks,
+}: {
+  moreLinks: { label: string; src: string }[];
+}) {
+  return (
+    <>
+      <Title my="l" size="h2" style={""}>
+        관련 자료
+      </Title>
+      {moreLinks.map((v, i) => (
+        <Text key={i}>
+          <Link
+            target="_blank"
+            href={v.src}
+            className=" flex flex-col hover:font-semibold hover:text-blue-900"
+          >
+            <div className="">
+              {i + 1}. {v.label} :{" "}
+            </div>
+            <div className="flex items-center  ">
+              <Link2 size="15" className="inline-block" />{" "}
+              <Text style=" text-ellipsis overflow-hidden">{v.src}</Text>
+            </div>
+          </Link>
+        </Text>
+      ))}
+    </>
+  );
 }
-
-
-
