e7f883b704f2e1a295319ca102961e6e7f50e154
Eric Kim <cyon256@icloud.com>
2026-05-10 23:52:42 +0900

    fix'

--- app/editor/page.tsx
+++ app/editor/page.tsx
@@ -1,9 +1,9 @@
 import TipTab from "@/components/editor/TipTab";
 
 export default function Page() {
-	return (
-		<div className="sm:w-sm md:w-md lg:w-lg xl:w-xl  p-5">
-			<TipTab editable={true} />
-		</div>
-	)
+  return (
+    <div className="w-full  p-5">
+      <TipTab editable={true} />
+    </div>
+  );
 }
